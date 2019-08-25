ccad722e4055f3ae336bfcfed850d50415c96195
Masayo Kimura <masaki@hautbois.jp>
2019-08-25 22:43:33 +0900

    added license

--- mspp/src/main/java/ninja/mspp/plugin/io/file/mzml_jmzml_wrapper/jmzMLUnmarshallerFactory.java
+++ mspp/src/main/java/ninja/mspp/plugin/io/file/mzml_jmzml_wrapper/jmzMLUnmarshallerFactory.java
@@ -1,8 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * BSD 3-Clause License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @author Mass++ Users Group
+ * @author Masaki Murase
+ * @since 2018-05-30 02:24:12+09:00
+ *
+ * Copyright (c) 2018, Mass++ Users Group
+ * All rights reserved.
  */
+
 package ninja.mspp.plugin.io.file.mzml_jmzml_wrapper;
 
 import java.util.HashMap;
@@ -35,21 +67,21 @@ public class jmzMLUnmarshallerFactory {
     
     public synchronized MzMLUnmarshaller getUnmarshaller(File file){
         String fullpath = file.getAbsolutePath();
-        MzMLUnmarshaller unmarshaller = pool.get(""+fullpath);
+        MzMLUnmarshaller unmarshaller = this.pool.get(""+fullpath);
         
         if(unmarshaller ==null){
             unmarshaller = new MzMLUnmarshaller(file);
-            pool.put(fullpath, unmarshaller);
+            this.pool.put(fullpath, unmarshaller);
         }         
         return unmarshaller;
     }
     
     public synchronized void removeUnmarshaller(File file){
-        pool.remove(file.getAbsolutePath());
+        this.pool.remove(file.getAbsolutePath());
     }
     
     public synchronized void clearAllUnmarshaller(){
-        pool.clear();
+        this.pool.clear();
     }
     
 }
